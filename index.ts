1bf87a5c44d5143503062f5b61fe777a44d0c051
Dawid Banachowski <bajkull@gmail.com>
2021-05-18 00:37:38 +0200

    heroku fix

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV == "development") require("dotenv").config();
+// if (process.env.NODE_ENV == "development") require("dotenv").config();
 
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const dotenv = require("dotenv-webpack");
 const webpack = require("webpack");
 
 module.exports = {
